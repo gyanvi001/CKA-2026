32be73a5500a8d8d084d991ec1244ac15864550f
gyanvi001 <gyanvipandey01@gmail.com>
2026-07-12 21:19:46 +0530

    Updated the role binding

---
+++ CKA Scenarios/Scenario-1/rolebinding.sh
@@ -0,0 +1,14 @@
+# create a service account named dev
+kubectl create sa dev
+
+# create a role binding named 'dev-view-binding' to allow the 'dev' service account to view resources in the default namespace
+kubectl create rolebinding dev-view-binding --clusterrole=view --serviceaccount=default:dev --namespace=default
+
+# verify the 'dev' service account can view pods in the default namespace
+kubectl auth can-i get po --namespace default --as=system:serviceaccount:default:dev
+
+# verify the 'dev' service account can view services in the default namespace
+kubectl auth can-i get svc --namespace default --as=system:serviceaccount:default:dev
+
+# verify that the 'dev' service account CANNOT view the pods in the 'kube-system' namespace
+kubectl auth can-i get po --namespace kube-system --as=system:serviceaccount:default:dev
